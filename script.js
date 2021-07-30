a19b91c9fb971fb61a225096f3e7877834b5ceb5
Alex <esarev@inbox.ru>
2021-07-30 19:22:33 +0700

    'easyLocStg'

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+//localStorage: setItem(ключ.значение),remove(), clear(),getItem();
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+if (localStorage.getItem('isChecked')) {
+  checkbox.checked = true;
+}
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
+
+  checkbox.addEventListener('change', () => {
+  localStorage.setItem('isChecked', true);
+});
+
+
+change.addEventListener('click', () => {
+  if (localStorage.getItem('bg') === 'changed') {
+    localStorage.removeItem('bg');
+    form.style.backgroundColor = '#fff';
+  } else {
+    localStorage.setItem('bg', 'changed');
+    form.style.backgroundColor = 'red';
+  }
+});
+
+
+const persone = {
+  name : 'Alex',
+  age: 32
+};
+const seriallizedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', seriallizedPersone);
+console.log(JSON.parse(localStorage.getItem('alex')));
